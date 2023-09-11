eb06ce7b6470c13d9fe7a89e31f7275e02a565b1
Sebastian Graef <sebastian.graef@novatec-gmbh.de>
2023-09-11 17:16:19 +0200

    add first tasks

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,6 +35,10 @@ const config = {
     ],
   },
 
+  markdown: {
+    mermaid: true,
+  },
+
   presets: [
     [
       "classic",
@@ -149,6 +153,8 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
+
+  themes: ["@docusaurus/theme-mermaid"],
 };
 
 module.exports = config;
